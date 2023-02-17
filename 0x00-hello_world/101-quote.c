75cf4e8b761f875bddb5bf4c8c6b0e30915319b8
cooljay202 <ezeebuakajames@gmail.com>
2023-02-16 18:28:07 -0800

    write 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+Include <unistd.h>
+/**
+*main - a progtrram that prints….
+*Return: 1 (success)
+*/
+Int main (void)
+{
+    
+write(2,
+	      "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+	      );
+
+
+	return(1);
